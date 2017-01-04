ca469811dd1a9325b5f83da0464e4a80e64ae753
Dave Junta <djunta@gmail.com>
2017-01-04 16:36:12 -0700

    Prep for review

--- app/models/calculations.rb
+++ app/models/calculations.rb
@@ -71,4 +71,16 @@ module Calculations
     ten_yr_cagr = (total_return ** ( 1 / years_held )) - 1
   end
 
+  def max_cagr(date)
+    # start_date = date.to_date
+    # end_date = (date.to_date + 10.years)
+    # start_price = PriceShiller.find_by(start_date).sp500_price
+    # end_price = PriceShiller.find_by(end_date).sp500_price
+    # divs_received = PriceShiller.where(date: start_date..end_date).sum(:mo_divs).to_f.round(2)
+    # total_return = (divs_received + end_price) / start_price
+    # years_held = (end_date - start_date) / 60 / 60 / 24 / 365.25
+    # ten_yr_cagr = (total_return ** ( 1 / years_held )) - 1
+  end
+
+
 end
