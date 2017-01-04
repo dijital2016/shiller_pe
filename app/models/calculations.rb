880083c4f155023ddb6d5953fc03dec7f7bfbbc6
Dave Junta <djunta@gmail.com>
2017-01-03 18:52:20 -0700

    Fixes cagr calcs

--- app/models/calculations.rb
+++ app/models/calculations.rb
@@ -1,4 +1,5 @@
 module Calculations
+
   def decile_shiller_pe(start, end)
     PriceShiller.where(:date => start..end).average(:shiller_pe).to_f.round(2)
   end
@@ -9,9 +10,9 @@ module Calculations
     start_price = PriceShiller.find_by(start_date).sp500_price
     end_price = PriceShiller.find_by(end_date).sp500_price
     divs_received = PriceShiller.where(date: start_date..end_date).sum(:mo_divs).to_f.round(2)
-    total_return = divs_received + end_price - start_price
+    total_return = (divs_received + end_price) / start_price
     years_held = (end_date - start_date) / 60 / 60 / 24 / 365.25
-    three_mo_cagr = total_return ** ( 1 / years_held )
+    three_mo_cagr = (total_return ** ( 1 / years_held )) - 1
   end
 
   def six_mo_cagr(date)
@@ -20,9 +21,9 @@ module Calculations
     start_price = PriceShiller.find_by(start_date).sp500_price
     end_price = PriceShiller.find_by(end_date).sp500_price
     divs_received = PriceShiller.where(date: start_date..end_date).sum(:mo_divs).to_f.round(2)
-    total_return = divs_received + end_price - start_price
+    total_return = (divs_received + end_price) / start_price
     years_held = (end_date - start_date) / 60 / 60 / 24 / 365.25
-    six_mo_cagr = total_return ** ( 1 / years_held )
+    six_mo_cagr = (total_return ** ( 1 / years_held )) - 1
   end
 
   def one_yr_cagr(date)
@@ -31,9 +32,9 @@ module Calculations
     start_price = PriceShiller.find_by(start_date).sp500_price
     end_price = PriceShiller.find_by(end_date).sp500_price
     divs_received = PriceShiller.where(date: start_date..end_date).sum(:mo_divs).to_f.round(2)
-    total_return = divs_received + end_price - start_price
+    total_return = (divs_received + end_price) / start_price
     years_held = (end_date - start_date) / 60 / 60 / 24 / 365.25
-    one_yr_cagr = total_return ** ( 1 / years_held )
+    one_yr_cagr = (total_return ** ( 1 / years_held )) - 1
   end
 
   def three_yr_cagr(date)
@@ -42,9 +43,9 @@ module Calculations
     start_price = PriceShiller.find_by(start_date).sp500_price
     end_price = PriceShiller.find_by(end_date).sp500_price
     divs_received = PriceShiller.where(date: start_date..end_date).sum(:mo_divs).to_f.round(2)
-    total_return = divs_received + end_price - start_price
+    total_return = (divs_received + end_price) / start_price
     years_held = (end_date - start_date) / 60 / 60 / 24 / 365.25
-    three_yr_cagr = total_return ** ( 1 / years_held )
+    three_yr_cagr = (total_return ** ( 1 / years_held )) - 1
   end
 
   def five_yr_cagr(date)
@@ -53,9 +54,9 @@ module Calculations
     start_price = PriceShiller.find_by(start_date).sp500_price
     end_price = PriceShiller.find_by(end_date).sp500_price
     divs_received = PriceShiller.where(date: start_date..end_date).sum(:mo_divs).to_f.round(2)
-    total_return = divs_received + end_price - start_price
+    total_return = (divs_received + end_price) / start_price
     years_held = (end_date - start_date) / 60 / 60 / 24 / 365.25
-    five_yr_cagr = total_return ** ( 1 / years_held )
+    five_yr_cagr = (total_return ** ( 1 / years_held )) - 1
   end
 
   def ten_yr_cagr(date)
@@ -64,11 +65,9 @@ module Calculations
     start_price = PriceShiller.find_by(start_date).sp500_price
     end_price = PriceShiller.find_by(end_date).sp500_price
     divs_received = PriceShiller.where(date: start_date..end_date).sum(:mo_divs).to_f.round(2)
-    total_return = divs_received + end_price - start_price
+    total_return = (divs_received + end_price) / start_price
     years_held = (end_date - start_date) / 60 / 60 / 24 / 365.25
-    ten_yr_cagr = total_return ** ( 1 / years_held )
+    ten_yr_cagr = (total_return ** ( 1 / years_held )) - 1
   end
-
-
 
 end
