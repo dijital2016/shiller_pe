18d60aed6ccbc132ffd5410e91519ca9b05f732a
Dave Junta <djunta@gmail.com>
2017-01-05 13:01:15 -0700

    Fixes root page feature tests

--- spec/features/user_visits_root_and_sees_welcome_spec.rb
+++ spec/features/user_visits_root_and_sees_welcome_spec.rb
@@ -5,10 +5,13 @@ describe "a user visits root page and sees welcome" do
     it "should see welcome" do
       visit "/"
 
-      expect(page).to have_content("Welcome")
+      expect(page).to have_content("Welcome!")
+      expect(page).to have_content("Shiller P/E")
+      expect(page).to have_content("Dividends")
       expect(page).to have_content("2016-11-01")
-      expect(page).to have_content("2097.94")
+      expect(page).to have_content("$2,097.94")
       expect(page).to have_content("2016-10-01")
+      expect(page).to have_content("26.50")
     end
   end
 end
