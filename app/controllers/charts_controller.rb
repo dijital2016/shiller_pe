2497c12b926fa2517bc4f8f458e6264b17162115
David Junta <djunta@gmail.com>
2017-04-03 18:38:21 -0600

    Adding 2nd chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,7 +1,16 @@
 class ChartsController < ApplicationController
-  
+
   def index
     @all_data = PriceShiller.all.order("date ASC")
+  end
+
+  def show
+    if DecilePerformance.count == 0
+      DecilePerformance.generate_decile_data
+    end
+
+    @sp_averages = 1
+    @performance_by_decile = DecilePerformance.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'data#index'
   get '/charts', to: 'charts#index'
-  get '/chartstest', to: 'charts#show'
+  get '/relativechart', to: 'charts#show'
   get '/decile_performance', to: 'decile_performance#index'
   get '/conclusions', to: 'conclusions#index'
 
